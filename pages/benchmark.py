f715fcff4ea15309337d1788ee558782d3fde092
g3ur3mugoto <g3ur3mugoto@gmail.com>
2024-02-02 16:55:35 +0900

    Fist commit

---
+++ pages/benchmark.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title('ORCA Benchmark')
+st.caption('Orca Benchmark Scripts from PySDKExamples')
+st.subheader('single_model_performance_test.ipynb')
+st.link_button('Script (Single Model)', 'https://colab.research.google.com/github/DeGirum/PySDKExamples/blob/main/examples/benchmarks/single_model_performance_test.ipynb')
+st.subheader('multi_model_performance_test.ipynb')
+st.link_button('Script (Multi Model)', 'https://colab.research.google.com/github/DeGirum/PySDKExamples/blob/main/examples/benchmarks/multi_model_performance_test.ipynb')
+
---
+++ top_page.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from PIL import Image
+#Text
+st.title('ORCA Test Plans')
+st.caption('Basic interoeprability checking procedure')
+st.subheader('Install PySDK')
+st.text('Install PySDK to the target system')
+
+#Image
+#image = Image.open('./object_detection.png')
+#st.image(image, width = 200)
