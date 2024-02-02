6152c6a88c9da0f611eab8e5c286c8d888a5c8fb
g3ur3mugoto <g3ur3mugoto@gmail.com>
2024-02-03 02:10:32 +0900

    Update 020324

---
+++ pages/02_benchmark.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+data_dir = '/home/gotom/orca_test_plans/orca_test_web_app/data/'
+
+st.title('ORCA Benchmark Test')
+st.caption('Orca Benchmark Scripts from PySDKExamples')
+#Single model benchmark
+st.subheader('single_model_performance_test.ipynb')
+st.link_button('Script', 'https://colab.research.google.com/github/DeGirum/PySDKExamples/blob/main/examples/benchmarks/single_model_performance_test.ipynb')
+
+st.text('Output Example')
+image = Image.open(data_dir + 'benchmark_single_model.png')
+st.image(image, width = 1000)
+
+
+#Multi model benckmark
+st.subheader('multi_model_performance_test.ipynb')
+st.link_button('Script', 'https://colab.research.google.com/github/DeGirum/PySDKExamples/blob/main/examples/benchmarks/multi_model_performance_test.ipynb')
+
+st.text('Output Example')
+image = Image.open(data_dir + 'benchmark_multi_model.png')
+st.image(image, width = 800)
+
+# single_model_benchmark = pd.read_table('/home/gotom/orca_test_plans/orca_test_web_app/data/benchmark_multi_model.txt')
+# df = pd.DataFrame(single_model_benchmark)
+# st.table(df)
