07374380d838ed8a49385fd2523033e9e6da7bc7
gotom <g3ur3mugoto@gmail.com>
2024-02-05 21:49:05 +0900

    Update 020424-3 (Windows)

--- pages/03_PySDKexamples.py
+++ pages/03_PySDKexamples.py
@@ -4,23 +4,23 @@ from PIL import Image
 data_dir = './data/'
 
 st.title('PySDKExamples output examples')
-st.caption('Running the scripts within PySDKExamples')
+st.caption('Running scripts within PySDKExamples')
 
 #Basic outputs
 st.subheader('Basic Exampels')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.text('object_detection_image.ipynb')
-    image = Image.open(data_dir + 'object_detection_image.png')
+    st.text('basic_pysdk_demo_image.ipynb')
+    image = Image.open(data_dir + 'basic_pysdk_demo_image.png')
     st.image(image, width = 300)
     st.link_button('Script', 'https://colab.research.google.com/github/DeGirum/PySDKExamples/blob/main/examples/basic/basic_pysdk_demo_image.ipynb')
     
 with col2:
-    st.text('object_detection_video_stream.ipynb')
-    image = Image.open(data_dir + 'object_detection_video.png')
+    st.text('basic_pysdk_demo_video_stream.ipynb')
+    image = Image.open(data_dir + 'basic_pysdk_demo_video_stream.png')
     st.image(image, width = 300)
-    st.link_button('Script', 'https://colab.research.google.com/github/DeGirum/PySDKExamples/blob/main/examples/singlemodel/object_detection_video_stream.ipynb')
+    st.link_button('Script', 'https://colab.research.google.com/github/DeGirum/PySDKExamples/blob/main/examples/basic/basic_pysdk_demo_video_stream.ipynb')
 
 #Specialized outputs
 st.subheader('Advanced Algorithms')
@@ -28,20 +28,25 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.text('multi_object_tracking_video_file.ipynb')
-    image = Image.open(data_dir + 'multi_object_tracking_video.png')
-    st.image(image, width = 350)
+    #image = Image.open(data_dir + 'multi_object_tracking_video.png')
+    #st.image(image, width = 350)
+    video_file = open(data_dir + 'multi_object_tracking_video_file.mp4', 'rb')
+    video_bytes = video_file.read()
+    st.video(video_bytes)
     st.link_button('Script', 'https://colab.research.google.com/github/DeGirum/PySDKExamples/blob/main/examples/specialized/multi_object_tracking_video_file.ipynb')
     
 with col2:
     st.text('object_detection_in_zone_counting_video_file.ipynb')
-    image = Image.open(data_dir + 'object_in_zone_counting_video.png')
-    st.image(image, width = 350)
+    video_file = open(data_dir + 'object_in_zone_counting_video_file.mp4', 'rb')
+    video_bytes = video_file.read()
+    st.video(video_bytes)
     st.link_button('Script', 'https://colab.research.google.com/github/DeGirum/PySDKExamples/blob/main/examples/singlemodel/object_detection_video_stream.ipynb')
 
 col1, col2 = st.columns(2)
 
 with col1:
     st.text('sliced_object_detection.ipynb')
-    image = Image.open(data_dir + 'sliced_object_detection.png')
-    st.image(image, width = 350)
+    video_file = open(data_dir + 'sliced_object_detection.mp4', 'rb')
+    video_bytes = video_file.read()
+    st.video(video_bytes)
     st.link_button('Script', 'https://colab.research.google.com/github/DeGirum/PySDKExamples/blob/main/examples/specialized/multi_object_tracking_video_file.ipynb')
--- top_page.py
+++ top_page.py
@@ -1,23 +1,29 @@
 import streamlit as st
 from PIL import Image
+import pandas as pd
 #Text
-st.title('ORCA Test Plans (Preliminary)')
+st.title('ORCA Test Plans for Target Device (Preliminary)')
 st.caption('Basic interoeprability checking procedure')
-st.subheader('Install')
-st.text('* Install ORCA driver')
-st.text('* Install PySDK')
+st.subheader('Installation')
+st.text('* ORCA driver (OS:Linux)')
+st.text('* PySDK')
 st.subheader('Benchmark')
-st.text('* Benchmark Single Model')
-st.text('* Benchmark Multi Model')
-st.subheader('PySDKExamples(basic)')
-st.text('* object_detection_image.ipynb')
-st.text('* object_detection_video_stream.ipynb')
-st.text('* Install PySDK')
-st.subheader('PySDKExamples(specialized)')
+st.text('* Single model')
+st.text('* Multi model')
+#st.text('* Benchmark (ORCA vs. EdgeTPU)')
+st.subheader('PySDKExamples')
+st.subheader('Basic')
+st.text('* basic_pysdk_demo_image.ipynb')
+st.text('* basic_pysdk_demo_video_stream.ipynb')
+st.subheader('Advanced algorithm')
 st.text('* multi_object_tracking__video_file.ipynb')
 st.text('* object_in_zone_counting_video_stream.ipynb')
 st.text('* sliced_object_detection.ipynb')
 
-#Image
-#image = Image.open('./object_detection.png')
-#st.image(image, width = 200)
+# test_categoriesL = ['Installation', 'Benchmark', 'Run PySDKExamples script']
+# scriptsL = ['NA', 'single_model_performance_test.ipynb'+'\n'+'single_model_performance_test.ipynb','']
+# remarksL = ['OS: Linux','','']
+
+# df = pd.DataFrame({'Test Category': test_categoriesL, 'Test Script': scriptsL, 'Remarks': remarksL})
+
+# st.dataframe(df, width=600)
